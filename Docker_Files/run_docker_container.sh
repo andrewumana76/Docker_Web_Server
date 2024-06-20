6b87aa0413e14f75e59cdda14b1a8448ad561696
Umanaandrew <umanaandrew95@gmail.com>
2024-06-19 22:14:49 -0400

    pushing changed files

--- Docker_Files/run_docker_container.sh
+++ Docker_Files/run_docker_container.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-docker run -d -p 8080:80 --name container_nginx_webserver image_nginx_webserver
+#-v :/usr/share/nginx/html
+
+website_path="/mnt/d/Git_Repos/Docker_Web_Server/Docker_Web_Server/Web_Source_Files"
+nginx_path="/usr/share/nginx/html"
+
+docker run -d -p 8080:80 --name container_nginx_webserver -v $website_path:$nginx_path image_nginx_webserver
