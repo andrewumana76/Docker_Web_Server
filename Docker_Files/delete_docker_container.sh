8a90c085a5c501b9625dc185af6d0ebaff01674d
Umanaandrew <umanaandrew95@gmail.com>
2024-06-23 22:52:31 -0400

    can now be accessed from public IP address

--- Docker_Files/delete_docker_container.sh
+++ Docker_Files/delete_docker_container.sh
@@ -2,5 +2,6 @@
 
 #This file is used when we need to remove the old container and image
 docker stop container_nginx_webserver
+docker network prune -f
 docker rm container_nginx_webserver
 docker image rm image_nginx_webserver
--- Docker_Files/run_docker_container.sh
+++ Docker_Files/run_docker_container.sh
@@ -3,7 +3,10 @@
 #-v :/usr/share/nginx/html
 
 website_path="/mnt/d/Git_Repos/Docker_Web_Server/Docker_Web_Server/Web_Source_Files/"
-#nginx_path="/usr/share/nginx/html"
 nginx_path="/etc/nginx/html"
 
-docker run -d -p 8080:80 --name container_nginx_webserver -v $website_path:$nginx_path image_nginx_webserver
+#create network
+docker network create --subnet=192.168.1.0/24 --gateway=192.168.1.1 my-network
+
+#runs the container
+docker run -d -p 80:80 --name container_nginx_webserver --network my-network --ip 192.168.1.231 --dns 192.168.1.244 -v $website_path:$nginx_path image_nginx_webserver
